3d939be6f176e7de289f7747e7a58db9330cc2a4
Michael <135025465+T8mpest@users.noreply.github.com>
2025-01-12 13:00:53 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TgBotForMedUniversity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
